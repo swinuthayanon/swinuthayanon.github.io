b1c02c92cd514b064a416b6fe214de7f87ce053b
Sarayut Winuthayanon <sarayut@ubuntu>
2020-03-15 01:19:30 -0700

    update github

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,42 +2,42 @@ var store = [{
         "title": "Data Wrangling Project",
         "excerpt":"H1 Heading H2 Heading H3 Heading Here’s some basic text. And here’s some italics Here’s some bold text. What about a link? Here’s a bulleted list: First item Second item Third item Here’s a numbered list: First Second Third Python code block: import numpy as np def test_function(x, y): z...","categories": [],
         "tags": ["data wrangling","data science","messy data"],
-        "url": "http://192.168.9.220:4000/perceptron/",
+        "url": "/perceptron/",
         "teaser": null
       },{
         "title": "I love coding!",
         "excerpt":"State of Highlighting Code in Jekyll I appear to be able to highlight syntax in Markdown files in two different ways using Jekyll. This `is not a code` span! \\ backslash . period * asterisk _ underscore + plus - minus = equal sign ` back tick ()[]{}&lt;&gt; left and...","categories": [],
         "tags": [],
-        "url": "http://192.168.9.220:4000/Code/",
+        "url": "/Code/",
         "teaser": null
       },{
         "title": "I love Math!",
         "excerpt":"Some inline Latex:    When $a \\ne 0$, there are two solutions to (ax^2 + bx + c = 0) and they are    Display equation:     These render differently. For example, type $\\sum_{i=0}^n i^2 = \\frac{(n^2+n)(2n+1)}{6}$ to show  (which is inline mode) or type  to show     ","categories": [],
         "tags": [],
-        "url": "http://192.168.9.220:4000/Math/",
+        "url": "/Math/",
         "teaser": null
       },{
         "title": "I love pizza!",
         "excerpt":"Hello Test  Nine  ","categories": [],
         "tags": [],
-        "url": "http://192.168.9.220:4000/Pizza/",
+        "url": "/Pizza/",
         "teaser": null
       },{
         "title": "About Math",
         "excerpt":"Some inline Latex:    When $a \\ne 0$, there are two solutions to (ax^2 + bx + c = 0) and they are    Display equation:     ","categories": [],
         "tags": [],
-        "url": "http://192.168.9.220:4000/About/",
+        "url": "/About/",
         "teaser": null
       },{
         "title": "Butterfly",
         "excerpt":"Butterfly animation. Butterfly animation.   ","categories": [],
         "tags": [],
-        "url": "http://192.168.9.220:4000/Butterfly/",
+        "url": "/Butterfly/",
         "teaser": null
       },{
         "title": "Giphy",
         "excerpt":"Giphy animation.  ","categories": [],
         "tags": [],
-        "url": "http://192.168.9.220:4000/Giphy/",
+        "url": "/Giphy/",
         "teaser": null
       }]
